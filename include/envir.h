e2dc0502c3c58c0b3c2eff334b3b4e1865126d0f
guilyx <erwin.lejeune15@gmail.com>
2019-09-30 19:32:55 +0200

    :tada: Initial skelletton from O. Kermorgan

---
+++ include/envir.h
@@ -0,0 +1,48 @@
+#ifndef ENVIR_H
+#define ENVIR_H
+
+#include <vector>
+#include <geom.h>
+
+namespace arpro
+{
+
+
+class Robot;
+
+class Environment
+{
+
+    std::vector<Pose> walls;
+    Pose target_;
+    std::vector<double> x_hist, y_hist;
+    std::vector<Robot*> robots_;
+
+    double dt = 0.1;
+    double t = 0;
+
+public:
+    Environment();
+
+    double time() const
+    {
+      return t;
+    }
+
+    // the target draws a cardoid curve
+    void updateTarget();
+
+    Pose target() const
+    {
+      return target_;
+    }
+
+    void addRobot(Robot &_robot);
+
+    // plots the trajectory in the given environment
+    void plot();
+};
+
+}
+
+#endif // ENVIR_H
